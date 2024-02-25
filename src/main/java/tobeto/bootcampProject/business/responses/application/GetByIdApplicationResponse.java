a5d89205f1a5c1ffe976cfe8e2b938f86c3f6f46
pc <htccnbz06@gmail.com>
2024-02-26 00:06:16 +0300

    w-3

---
+++ src/main/java/tobeto/bootcampProject/business/responses/application/GetByIdApplicationResponse.java
@@ -0,0 +1,12 @@
+package tobeto.bootcampProject.business.responses.application;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+public class GetByIdApplicationResponse {
+    private int id;
+    private int applicantId;
+    private int bootcampId;
+    private int getApplicantId;
+}
